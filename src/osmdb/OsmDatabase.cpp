b9d7380686483aaf9c774b1ba88db9cd58ee71e0
Martin Kolinek <martin.kolinek@gmail.com>
2011-11-12 23:01:47 +0100

    OsmDatabase now has public member functions to get index creating statements

--- src/osmdb/OsmDatabase.cpp
+++ src/osmdb/OsmDatabase.cpp
@@ -19,23 +19,23 @@ OsmDatabase::OsmDatabase(const std::string& file)
       relation_contents_table("RelationContents"),
       attributes_table("Attributes"),
       db(file),
-      nodes_create("CREATE TABLE " + nodes_table + " (ID INTEGER NOT NULL PRIMARY KEY, Latitude REAL NOT NULL, Longitude REAL NOT NULL)"),
-      ways_create("CREATE TABLE " + ways_table + " (ID INTEGER NOT NULL PRIMARY KEY)"),
-      edges_create("CREATE TABLE " + edges_table + " (ID INTEGER NOT NULL PRIMARY KEY, WayID INTEGER NOT NULL, StartNodeID INTEGER NOT NULL, EndNodeID INTEGER NOT NULL)"),
-      relations_create("CREATE TABLE " + relations_table + " (ID INTEGER NOT NULL PRIMARY KEY)"),
-      relation_contents_create("CREATE TABLE " + relation_contents_table + " (ID INTEGER NOT NULL PRIMARY KEY, RelationID INTEGER NOT NULL, Role TEXT NOT NULL, ObjectID INTEGER NOT NULL, ObjectType INTEGER NOT NULL)"),
-      attributes_create("CREATE TABLE " + attributes_table + " (ID INTEGER NOT NULL PRIMARY KEY, ObjectID INTEGER NOT NULL, ObjectType INTEGER NOT NULL, Key TEXT, Value TEXT)"),
-      attr_index1("CREATE INDEX IX_Attributes_Object ON " + attributes_table + "(ObjectID, ObjectType)"),
-      attr_index2("CREATE INDEX IX_Attributes_Key ON " + attributes_table + "(Key)"),
-      attr_index3("CREATE INDEX IX_Attributes_Value ON " + attributes_table + "(Value)"),
-      edge_index_start("CREATE INDEX IX_Edges_Start ON " + edges_table + "(StartNodeID)"),
-      edge_index_end("CREATE INDEX IX_Edges_End ON " + edges_table + "(EndNodeID)"),
-      edge_index_way("CREATE INDEX IX_Edges_Way ON " + edges_table + "(WayID)"),
-      node_index_lat("CREATE INDEX IX_Nodes_Lat ON " + nodes_table + "(Latitude)"),
-      node_index_lon("CREATE INDEX IX_Nodes_Lon ON " + nodes_table + "(Longitude)"),
-      rel_cont_index_obj("CREATE INDEX IX_RelationContents_Object ON " + relation_contents_table + "(ObjectID, ObjectType)"),
-      rel_cont_index_rel("CREATE INDEX IX_RelationContents_Relation ON " + relation_contents_table + "(RelationID)"),
-      rel_cont_index_role("CREATE INDEX IX_RelationContents_Role ON " + relation_contents_table + "(Role)"),
+      nodes_create(get_nodes_create(nodes_table)),
+      ways_create(get_ways_create(ways_table)),
+      edges_create(get_edges_create(edges_table)),
+      relations_create(get_relations_create(relations_table)),
+      relation_contents_create(get_relation_contents_create(relation_contents_table)),
+      attributes_create(get_attributes_create(attributes_table)),
+      attr_index1(get_attr_index1(attributes_table)),
+      attr_index2(get_attr_index2(attributes_table)),
+      attr_index3(get_attr_index3(attributes_table)),
+      edge_index_start(get_edge_index_start(edges_table)),
+      edge_index_end(get_edge_index_end(edges_table)),
+      edge_index_way(get_edge_index_way(edges_table)),
+      node_index_lat(get_node_index_lat(nodes_table)),
+      node_index_lon(get_node_index_lon(nodes_table)),
+      rel_cont_index_obj(get_rel_cont_index_obj(relation_contents_table)),
+      rel_cont_index_rel(get_rel_cont_index_rel(relation_contents_table)),
+      rel_cont_index_role(get_rel_cont_index_role(relation_contents_table)),
       nodes_test("SELECT ID, Latitude, Longitude FROM " + nodes_table + " LIMIT 1"),
       ways_test("SELECT ID FROM " + ways_table + " LIMIT 1"),
       edge_test("SELECT WayID, StartNodeID, EndNodeID FROM " + edges_table + " LIMIT 1"),
@@ -81,6 +81,101 @@ void OsmDatabase::create_tables()
     {
         sqlite::execute_sql(*it, db);
     }
+}
+
+std::string OsmDatabase::get_nodes_create(const std::string& nodes_table)
+{
+    return "CREATE TABLE " + nodes_table + " (ID INTEGER NOT NULL PRIMARY KEY, Latitude REAL NOT NULL, Longitude REAL NOT NULL)";
+}
+
+std::string OsmDatabase::get_ways_create(const std::string& ways_table)
+{
+    return "CREATE TABLE " + ways_table + " (ID INTEGER NOT NULL PRIMARY KEY)";
+}
+
+std::string OsmDatabase::get_edges_create(const std::string& edges_table)
+{
+    return "CREATE TABLE " + edges_table + " (ID INTEGER NOT NULL PRIMARY KEY, WayID INTEGER NOT NULL, StartNodeID INTEGER NOT NULL, EndNodeID INTEGER NOT NULL)";
+}
+
+std::string OsmDatabase::get_relations_create(const std::string& relations_table)
+{
+    return "CREATE TABLE " + relations_table + " (ID INTEGER NOT NULL PRIMARY KEY)";
+}
+
+std::string OsmDatabase::get_relation_contents_create(const std::string& relation_contents_table)
+{
+    return "CREATE TABLE " + relation_contents_table + " (ID INTEGER NOT NULL PRIMARY KEY, RelationID INTEGER NOT NULL, Role TEXT NOT NULL, ObjectID INTEGER NOT NULL, ObjectType INTEGER NOT NULL)";
+}
+
+std::string OsmDatabase::get_attributes_create(const std::string& attributes_table)
+{
+    return "CREATE TABLE " + attributes_table + " (ID INTEGER NOT NULL PRIMARY KEY, ObjectID INTEGER NOT NULL, ObjectType INTEGER NOT NULL, Key TEXT, Value TEXT)";
+}
+
+std::string OsmDatabase::get_attr_index1(const std::string& attributes_table)
+{
+    return "CREATE INDEX IX_Attributes_Object ON " + attributes_table + "(ObjectID, ObjectType)";
+
+}
+
+std::string OsmDatabase::get_attr_index2(const std::string& attributes_table)
+{
+    return "CREATE INDEX IX_Attributes_Key ON " + attributes_table + "(Key)";
+
+}
+
+std::string OsmDatabase::get_attr_index3(const std::string& attributes_table)
+{
+    return "CREATE INDEX IX_Attributes_Value ON " + attributes_table + "(Value)";
+
+}
+
+std::string OsmDatabase::get_edge_index_start(const std::string& edges_table)
+{
+    return "CREATE INDEX IX_Edges_Start ON " + edges_table + "(StartNodeID)";
+
+}
+
+std::string OsmDatabase::get_edge_index_end(const std::string& edges_table)
+{
+    return "CREATE INDEX IX_Edges_End ON " + edges_table + "(EndNodeID)";
+
+}
+
+std::string OsmDatabase::get_edge_index_way(const std::string& edges_table)
+{
+    return "CREATE INDEX IX_Edges_Way ON " + edges_table + "(WayID)";
+
+}
+
+std::string OsmDatabase::get_node_index_lat(const std::string& nodes_table)
+{
+    return "CREATE INDEX IX_Nodes_Lat ON " + nodes_table + "(Latitude)";
+
+}
+
+std::string OsmDatabase::get_node_index_lon(const std::string& nodes_table)
+{
+    return "CREATE INDEX IX_Nodes_Lon ON " + nodes_table + "(Longitude)";
+
+}
+
+std::string OsmDatabase::get_rel_cont_index_obj(const std::string& relation_contents_table)
+{
+    return "CREATE INDEX IX_RelationContents_Object ON " + relation_contents_table + "(ObjectID, ObjectType)";
+
+}
+
+std::string OsmDatabase::get_rel_cont_index_rel(const std::string& relation_contents_table)
+{
+    return "CREATE INDEX IX_RelationContents_Relation ON " + relation_contents_table + "(RelationID)";
+
+}
+
+std::string OsmDatabase::get_rel_cont_index_role(const std::string& relation_contents_table)
+{
+    return "CREATE INDEX IX_RelationContents_Role ON " + relation_contents_table + "(Role)";
 }
 
 void OsmDatabase::check_tables()
--- src/osmdb/OsmDatabase.h
+++ src/osmdb/OsmDatabase.h
@@ -43,6 +43,25 @@ public:
      * Call after inserting most of the data.
      */
     void create_indexes();
+
+    std::string get_nodes_create(std::string const& nodes_table);
+    std::string get_ways_create(std::string const& ways_table);
+    std::string get_edges_create(std::string const& edges_table);
+    std::string get_relations_create(std::string const& relations_table);
+    std::string get_relation_contents_create(std::string const& relation_contents_table);
+    std::string get_attributes_create(std::string const& attributes_table);
+
+    std::string get_attr_index1(std::string const& attributes_table);
+    std::string get_attr_index2(std::string const& attributes_table);
+    std::string get_attr_index3(std::string const& attributes_table);
+    std::string get_edge_index_start(std::string const& edges_table);
+    std::string get_edge_index_end(std::string const& edges_table);
+    std::string get_edge_index_way(std::string const& edges_table);
+    std::string get_node_index_lat(std::string const& nodes_table);
+    std::string get_node_index_lon(std::string const& nodes_table);
+    std::string get_rel_cont_index_obj(std::string const& relation_conents_table);
+    std::string get_rel_cont_index_rel(std::string const& relation_conents_table);
+    std::string get_rel_cont_index_role(std::string const& relation_conents_table);
 private:
     sqlite::Database db;
     std::string const nodes_create;
