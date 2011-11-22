2011282b6fd77b580ed280449fe0cff813147449
Martin Kolinek <martin.kolinek@gmail.com>
2011-11-22 21:03:40 +0100

    Added BindTypes

---
+++ src/psql/BindTypes.cpp
@@ -0,0 +1,13 @@
+/*
+ * BindTypes.cpp
+ *
+ *  Created on: Nov 22, 2011
+ *      Author: martin
+ */
+
+#include "BindTypes.h"
+
+namespace psql
+{
+
+} /* namespace psql */
---
+++ src/psql/BindTypes.h
@@ -0,0 +1,55 @@
+/*
+ * BindTypes.h
+ *
+ *  Created on: Nov 22, 2011
+ *      Author: martin
+ */
+
+#ifndef BINDTYPES_H_
+#define BINDTYPES_H_
+
+#include "PqTypeWrap.h"
+
+namespace psql
+{
+
+template<typename... Args>
+class BindTypes
+{
+};
+
+template<typename Head, typename... Tail>
+class BindTypes<Head, Tail...> : private BindTypes<Tail...>
+{
+private:
+    PqTypeWrap<Head> tw;
+protected:
+    void put_prot(PGparam* param, Head h, Tail... t)
+    {
+        tw.put(param, h);
+        BindTypes<Tail...>::put_prot(param, t...);
+    }
+public:
+    void put(PGparam* param, Head h, Tail... t)
+    {
+        PQparamClear(param);
+        put_prot(param, h, t...);
+    }
+};
+
+template<>
+class BindTypes<>
+{
+protected:
+    void put_prot(PGparam*)
+    {
+    }
+public:
+    void put(PGparam* param)
+    {
+        PQparamClear(param);
+    }
+};
+
+} /* namespace psql */
+#endif /* BINDTYPES_H_ */
